870cdc57830d68e3e0e61c145773345576b9037f
XPRISTO <XPRISTO@DESKTOP-G2ROET4>
2024-10-30 00:31:09 +0200

    Add project files.

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using session2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session2.Context
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=efsession2;Trusted_Connection=true;TrustServerCertificate=true");
+
+        }
+        public DbSet<Employee> employees { get; set; }
+        public DbSet<Department> department { get; set; }
+    }
+}
---
+++ Migrations/20241029130035_updateEmployeeColumn.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace session2.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateEmployeeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "employees",
+                newName: "EmployeeName");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "employees",
+                newName: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EmployeeName",
+                table: "employees",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "EmployeeId",
+                table: "employees",
+                newName: "Id");
+        }
+    }
+}
